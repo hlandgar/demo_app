b5ebd65868af3ba6d6b205b1b7bdbf9e34e54456
Harris Landgarten <harris@tradeanedge.com>
2012-08-04 22:46:46 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '7944d1a54d138c5f9997a5f97cdee51b82134270f6208c98f0e23d6206f7e8de8a37db73a4bf9234db3fb3a014a3aeb230195021b203b22333096415a0b0631d'
